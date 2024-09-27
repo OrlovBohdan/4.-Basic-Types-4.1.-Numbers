049b051419b8283a183417426b8628cfb8ab0fe4
OrlovBohdan <b.orlov@e-u.edu.ua>
2024-09-27 12:41:44 +0300

    0

---
+++ src/task4.rs
@@ -0,0 +1,23 @@
+#[test]
+
+/*// Fill the blanks to make it work
+fn main() {
+    assert_eq!(i8::MAX, __);
+    assert_eq!(u8::MAX, __);
+
+    println!("Success!");
+}*/
+
+// Fill the blanks to make it work
+fn main() {
+    assert_eq!(i8::MAX, 127);
+    assert_eq!(u8::MAX, 255);
+
+    println!("Success!");
+}
+
+/*В этом коде нужно заполнить пропуски значениями максимальных чисел для типов i8 (знаковый 8-битный целочисленный тип)
+ и u8 (беззнаковый 8-битный целочисленный тип).
+
+Максимальное значение для типа i8 — это 127.
+Максимальное значение для типа u8 — это 255.*/
